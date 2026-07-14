7e0e0afb959ec8652a300c0272948493e8a72360
kopidinosaur <25011043@myrp.edu.sg>
2026-07-14 15:32:31 +0800

    deleted lines

--- app.js
+++ app.js
@@ -4,20 +4,3 @@ Jiajun
 
 HELLO TEST AGAIN
 
-<<<<<<< HEAD
-khoon
-
-hehe
-lalala
-
-
-hhhsss
-=======
-hehhhehhehehehehehehehe
-
-asdjbvgvhavisabsabod
-
-ok
->>>>>>> 4858cd5968c2fabd6792d0190632282aa627ee0e
-
-helloabc
