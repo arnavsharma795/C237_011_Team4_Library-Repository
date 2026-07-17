eee9dd283e541e9b4c8253d3afcb3fe75bd7d366
Levonnelim <Levonnelimz@gmail.com>
2026-07-17 23:34:28 +0800

    const PORT

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ app.get('/logout', (req,res) => {
     res.redirect('/');
 })
 // Starting the server
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
